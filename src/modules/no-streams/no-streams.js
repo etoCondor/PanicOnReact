18aacf7134101e065e37399f8506d86e8aa5fe91
Alex <fantomer2007@mail.ru>
2021-03-21 20:28:30 +0300

    style improvements

--- src/modules/no-streams/no-streams.js
+++ src/modules/no-streams/no-streams.js
@@ -4,7 +4,7 @@ const NoStreams = () => {
   return (
     <div className="errorCard">
       <img src={sadPepe} alt="sad Pepe" className="errorImage"></img>
-      <h2>"Увы, в данный момент никто из наших игроков не стримит :("</h2>
+      <h2>Увы, в данный момент никто из наших игроков не стримит :(</h2>
     </div>
   );
 };
